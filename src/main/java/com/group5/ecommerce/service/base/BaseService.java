89b84a48a78ec5fd9befaedd51e1284f82ac3f90
Emerzon Javier Kolki Martinez <emerzonkolki@gmail.com>
2023-04-22 21:14:39 -0500

    feat: delete example base routes

--- src/main/java/com/group5/ecommerce/service/base/BaseService.java
+++
@@ -1,8 +0,0 @@
-package com.group5.ecommerce.service.base;
-
-import com.group5.ecommerce.response.PongResponse;
-
-public interface BaseService {
-    PongResponse getPong();
-    PongResponse sendMessage(String message);
-}
