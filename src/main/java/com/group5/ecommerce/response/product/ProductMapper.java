de079d2accc13fdef9fa99a0e7f0c14bfabaf4a8
Emerzon Javier Kolki Martinez <emerzonkolki@gmail.com>
2023-04-25 20:47:27 -0500

    fix(ProductMapper): remove unmappedSourcePolicy

--- src/main/java/com/group5/ecommerce/response/product/ProductMapper.java
+++ src/main/java/com/group5/ecommerce/response/product/ProductMapper.java
@@ -7,7 +7,7 @@ import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 import org.mapstruct.factory.Mappers;
 
-@Mapper(componentModel = "spring", unmappedSourcePolicy = ReportingPolicy.IGNORE)
+@Mapper(componentModel = "spring")
 public interface ProductMapper {
 
     ProductMapper INSTANCE = Mappers.getMapper(ProductMapper.class);
