a52a01abd92fabe02b555664bc45939546ff28d9
Emerzon Javier Kolki Martinez <emerzonkolki@gmail.com>
2023-04-28 00:59:21 -0500

    feat(repositories): add findByName method
    add findByName in BrandRepository, CategoryRepository and ColorRepository

--- src/main/java/com/group5/ecommerce/repository/CategoryRepository.java
+++ src/main/java/com/group5/ecommerce/repository/CategoryRepository.java
@@ -1,9 +1,13 @@
 package com.group5.ecommerce.repository;
 
 import com.group5.ecommerce.entity.Category;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Optional<Category> findByName(String name);
 }
