110cfa13205b3f48972f3a95a8059f47ce1f66d1
Emerzon Javier Kolki Martinez <emerzonkolki@gmail.com>
2023-04-28 14:17:16 -0500

    fix(ProductMapper): update toListResponse

--- src/main/java/com/group5/ecommerce/response/product/ProductMapper.java
+++ src/main/java/com/group5/ecommerce/response/product/ProductMapper.java
@@ -3,13 +3,16 @@ package com.group5.ecommerce.response.product;
 import com.group5.ecommerce.entity.Image;
 import com.group5.ecommerce.entity.Product;
 
+import com.group5.ecommerce.response.brand.BrandResponse;
+import com.group5.ecommerce.response.category.CategoryResponse;
+import com.group5.ecommerce.response.color.ColorResponse;
 import com.group5.ecommerce.response.image.ImageResponse;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.Named;
 import org.mapstruct.factory.Mappers;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public interface ProductMapper {
@@ -18,11 +21,41 @@ public interface ProductMapper {
 
     DetailProductResponse toResponse(Product product);
 
-    @Mapping(source = "images", target = "image", qualifiedByName = "imageToImageResponse")
-    List<ProductResponse> toListResponse(List<Product> products);
+    default List<ProductResponse> toListResponse(List<Product> products) {
+        return products.stream()
+                .map(product -> new ProductResponse(
+                        product.getId(),
+                        product.getTitle(),
+                        product.getOriginalPrice(),
+                        product.getDiscountRate(),
+                        product.getQuantity(),
+                        product.getPublicationDate(),
+                        new CategoryResponse(
+                                product.getCategory().getId(),
+                                product.getCategory().getName()
+                        ),
+                        new BrandResponse(
+                                product.getBrand().getId(),
+                                product.getBrand().getName(),
+                                product.getBrand().getLogo()
+                        ),
+                        new ImageResponse(
+                                product.getImages().get(0).getId(),
+                                product.getImages().get(0).getImageUrl()
+                        ),
+                        product.getColors().stream()
+                                .map(color -> new ColorResponse(
+                                        color.getId(),
+                                        color.getName(),
+                                        color.getHex()
+                                ))
+                                .collect(Collectors.toList())
+                ))
+                .collect(Collectors.toList());
+    }
 
-    @Named("imageToImageResponse")
-    default ImageResponse imageToImageResponse(List<Image> images) {
+    @Named("imagesToImageResponse")
+    default ImageResponse imagesToImageResponse(List<Image> images) {
         return images.stream().findFirst()
                 .map(image -> new ImageResponse(image.getId(), image.getImageUrl()))
                 .orElse(null);
