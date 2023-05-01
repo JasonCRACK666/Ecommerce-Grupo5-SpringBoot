7bf4031460b4bc2705b61db7fc5353848bdaf675
Emerzon Javier Kolki Martinez <emerzonkolki@gmail.com>
2023-04-30 19:16:52 -0500

    feat: add send email util

---
+++ src/main/java/com/group5/ecommerce/config/JavaMailSenderConfig.java
@@ -0,0 +1,32 @@
+package com.group5.ecommerce.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class JavaMailSenderConfig {
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("sandbox.smtp.mailtrap.io");
+        mailSender.setPort(2525);
+
+        mailSender.setUsername("4f161313d97712");
+        mailSender.setPassword("d55c6617bb8b6e");
+
+        Properties props = mailSender.getJavaMailProperties();
+
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+
+}
