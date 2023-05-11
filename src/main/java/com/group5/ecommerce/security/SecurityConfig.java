2a3a1d65d3bf50eaa10a91e2acc5dda2e7c119dd
Emerzon Javier Kolki Martinez <emerzonkolki@gmail.com>
2023-05-11 18:56:32 -0500

    fix: remove cors

--- src/main/java/com/group5/ecommerce/security/SecurityConfig.java
+++ src/main/java/com/group5/ecommerce/security/SecurityConfig.java
@@ -26,8 +26,6 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .cors()
-                .disable()
                 .csrf()
                 .disable()
                 .authorizeHttpRequests((authz) -> authz
